bc5bea4724d27c2deefdaf37d582936a2b76d14a
ebinhon <bin.hong@ericsson.com>
2017-11-28 11:25:34 +0800

    add leanengine

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
+var AV = require('leanengine')
 var config = require('./webpack.dev.config')
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID || '4vl3Ec7FdH0u7MHG299SjF1Y-gzGzoHsz',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'z8AK3qYrctYaVdSTWtAMKbP0',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'QhzeT3Bunemox1kX9OQIAO4A'
+});
 
 var app = express()
 var compiler = webpack(config)
+
+app.use(AV.express())
 
 app.use(
   require('webpack-dev-middleware')(compiler, {
@@ -16,7 +25,7 @@ app.use(
 app.use(require('webpack-hot-middleware')(compiler))
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'src/index.html'))
+  res.sendFile(path.join(__dirname, 'build/index.html'))
 })
 
 app.listen(3003, 'localhost', function(err) {
--- src/leancloud/storage.js
+++ src/leancloud/storage.js
@@ -2,7 +2,7 @@
  * @Author: ebinhon 
  * @Date: 2017-11-24 16:54:56 
  * @Last Modified by: ebinhon
- * @Last Modified time: 2017-11-27 15:18:49
+ * @Last Modified time: 2017-11-28 11:24:09
  */
 import order from './models/order'
 import AV from 'leancloud-storage'
@@ -12,6 +12,7 @@ const APP_KEY = 'z8AK3qYrctYaVdSTWtAMKbP0'
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY,
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'QhzeT3Bunemox1kX9OQIAO4A',
 })
 
 AV.Object.register(order)
