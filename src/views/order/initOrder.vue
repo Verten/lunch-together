c755b5f1d03d5f6fa705abe1a0d53ff271ad5a91
ebinhon <bin.hong@ericsson.com>
2017-11-28 15:18:38 +0800

    css updated

--- src/views/order/initOrder.vue
+++ src/views/order/initOrder.vue
@@ -3,12 +3,12 @@
     <div>
       <h3 class="form-title">Order Information</h3>
       <div class="form">
+        <label for="orderName">Order Name</label><input type="text" placeholder="Order Name" name="orderName" id="orderName" v-model="order.name">
+        <label for="orderName">Order Price</label><input type="number" placeholder="Order Price" name="orderPrice" id="orderPrice" v-model.number="order.price">
+        <label for="orderName">Order Amount</label><input type="number" placeholder="Order Amount" name="orderAmount" id="orderAmount" v-model.number="order.amount">
+        <button type="button" class="second-button" @click="postOrder">Submit</button>
         <div class="success" v-if="info !== null">{{info.rawMessage}}</div>
         <div class="error" v-if="error !== null">{{error.rawMessage}}</div>
-        <label for="orderName">Order Name</label><input type="text" placeholder="Order Name" name="orderName" id="orderName" v-model="order.name">
-        <label for="orderName">Order Price</label><input type="text" placeholder="Order Price" name="orderPrice" id="orderPrice" @change="handleNumber" v-model="order.price">
-        <label for="orderName">Order Amount</label><input type="text" placeholder="Order Amount" name="orderAmount" id="orderAmount" @change="handleNumber" v-model="order.amount">
-        <button type="button" class="second-button" @click="postOrder">Submit</button>
       </div>
     </div>
     <div>
@@ -23,6 +23,7 @@
 <script>
 /* eslint no-undef: 0 */
 import { mapActions, mapGetters } from 'vuex'
+import { isEmpty } from 'lodash'
 
 export default {
   name: 'orderInit',
@@ -54,19 +55,23 @@ export default {
         $('h3 > span').addClass('close')
       }
     },
-    handleNumber(e) {
-      if (e.target.id === 'orderPrice') {
-        this.order.price = e.target.value
-      } else if (e.target.id === 'orderAmount') {
-        this.order.amount = e.target.value
-      }
-    },
     postOrder() {
-      this.order.owner = this.getOwn
+      this.order.owner = this.getOwn.username
+      if (isEmpty(this.order.name)) {
+        this.error = {
+          rawMessage: 'please input order name',
+        }
+        return false
+      }
       this.saveOrder(this.order).then(
         savedOrder => {
           this.info = {
             rawMessage: 'success',
+          }
+          this.order = {
+            name: '',
+            price: 0,
+            amount: 0,
           }
         },
         error => {
