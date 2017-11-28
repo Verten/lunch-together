15c736df152ad31ca060b1c012b300f117048e08
ebinhon <bin.hong@ericsson.com>
2017-11-28 11:46:31 +0800

    split dev and prod

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var path = require('path')
 var express = require('express')
-var webpack = require('webpack')
 var AV = require('leanengine')
-var config = require('./webpack.dev.config')
 
 AV.init({
   appId: process.env.LEANCLOUD_APP_ID || '4vl3Ec7FdH0u7MHG299SjF1Y-gzGzoHsz',
@@ -11,21 +9,13 @@ AV.init({
 });
 
 var app = express()
-var compiler = webpack(config)
 
 app.use(AV.express())
 
-app.use(
-  require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath,
-  })
-)
+app.use('/', express.static(__dirname + '/build'));
 
-app.use(require('webpack-hot-middleware')(compiler))
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build/index.html'))
+app.get('/*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
 
 app.listen(3003, function(err) {
@@ -33,6 +23,5 @@ app.listen(3003, function(err) {
     console.log(err)
     return
   }
-
   console.log('Listening at http://localhost:3003')
 })
